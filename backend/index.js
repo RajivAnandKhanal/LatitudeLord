1215bb52f54947f14f8519edac3f790023947130
Rajiv Khanal <rajivkhanal781@gmail.com>
2026-05-25 13:00:03 +0545

    fix: dependency error

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { connectToMongoDb } = require("./connection");
+const { connectToMongoDb } = require("./db-connect");
 const app = express();
 const PORT = 8080;
 
